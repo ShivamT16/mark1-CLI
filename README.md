27045eefdd8ae76e74aec484230997f78a25d6f5
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2022-10-04 21:36:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+CLI based Quiz
+
+This is a CLI based Fun-Quiz on Shivam (the owner of this repl 😂).
+
+It asks some questions to the users and allow them to answers.
+Takes the answers as input and shows them their score.
+
+In the end, it shows the final score.
+
---
+++ index.js
@@ -0,0 +1,87 @@
+readlineSync = require("readline-sync")
+
+var score = 0;
+var questions = [{
+  question: "His favourite sabzi? ",
+  answer: "Ladyfinger"
+}, {
+  question: "Shivam's favourite movie? ",
+  answer: "Bahubali"
+},
+{
+  question: "Shivam's favourite SUV? ",
+  answer: "Scorpio"
+},
+{
+  question: "First programming language he learned? ",
+  answer: "C++"
+},
+{
+  question: "Shivam is graduate now? ",
+  answer: "Yes"
+}
+];
+
+var Username = readlineSync.question("May I know your name? ")
+console.log("Welcome " + Username)
+console.log("-------------")
+
+var Age = readlineSync.question("Am I older than 25? ")
+if (Age == "Yes" || Age == "yes") {
+  score = score - 1;
+  console.log("No,You are wrong. I'm 22 right now", "Your score is " + score)
+  console.log("-------------")
+}
+else {
+  score = score + 1;
+  console.log("You are right", "Your score is " + score)
+  console.log("-------------")
+}
+
+var questionss = " Where Shivam lives? "
+var answerss = "Noida"
+play(questionss, answerss)
+
+function play(question, answer) {
+  var City = readlineSync.question("Where shivam lives? ")
+
+  if (City === answer) {
+    score = score + 1;
+    console.log("You are right:", "Your score is " + score)
+    console.log("-------------")
+  }
+  else {
+    score = score - 1;
+    console.log("You are wrong:", "Your score is " + score)
+    console.log("-------------")
+  }
+}
+
+function plays(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("You are right");
+    score = score + 1;
+
+  } else {
+    score = score - 1;
+    console.log("You are wrong");
+
+  }
+  console.log("Your score is " + score);
+  console.log("-------------")
+}
+
+function game() {
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    plays(currentQuestion.question, currentQuestion.answer)
+  }
+}
+game();
+
+console.log("Your final score is: " + score);
+
+
+
